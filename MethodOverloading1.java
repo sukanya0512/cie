e6b158af2751168b39675e9bce76561f029400fb
sukanya0512 <sukanyamulimani05@gmail.com>
2025-04-09 11:00:56 +0530

    second

--- MethodOverloading1.java
+++ MethodOverloading1.java
@@ -1,20 +1,18 @@
-public class MethodOverloading1 {    
-      
-      static void display(int a) {  
-          System.out.println("Value of a is: " + a);  
-      }  
-  
-       
-      void display(int a, int b) {  
-          System.out.println("Value of a and b is: " + a + " and " + b);  
-      }  
-  
-     
-      public static void main(String[] args) {  
-          display(5);  
-  
-          MethodOverloading1 obj = new MethodOverloading1();  
-          obj.display(5, 7); 
-      }  
-  }
-  
+public class MethodOverloading1 {
+
+      static void display(int a) {
+            System.out.println("Value of a is: " + a);
+      }
+
+      void display(int a, int b) {
+            System.out.println("Value of a and b is: " + a + " and " + b);
+      }
+
+      public static void main(String[] args) {
+            display(5);
+
+            MethodOverloading1 obj = new MethodOverloading1();
+            obj.display(5, 7);
+            System.out.println("new line");
+      }
+}
